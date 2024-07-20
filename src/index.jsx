149cb2f5917b8df68f4a446915176016d80cabab
Florian Bode <florianbode59@gmail.com>
2024-07-20 21:53:32 +0200

    Routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import FicheLogement from './pages/FicheLogement'
+import About from './pages/About'
+import Error from './pages/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/logement/:idLogement" element={<FicheLogement />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+)

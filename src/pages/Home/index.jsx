416763fbcb9233603c12e9358bea688a85773efc
Florian Bode <florianbode59@gmail.com>
2024-07-21 19:20:32 +0200

    useFetch

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 import Banner from '../../components/Banner'
 import Card from '../../components/Card'
 
-import logements from './../../datas/logements.json'
+import useFetch from '../../utils/hooks/useFetch'
 
-import banner1 from './../../assets/img/Banner/banner1.png'
+import banner1 from '../../assets/img/Banner/banner1.png'
 
 const StyledHome = styled.div`
   width: 100%;
@@ -23,20 +23,35 @@ const StyledLogements = styled.div`
 `
 
 function Home() {
+  const {
+    data: logements,
+    isLoading,
+    error,
+  } = useFetch('/datas/logements.json')
+
   return (
     <StyledHome>
       <Banner texte="Chez vous, partout et ailleurs" picture={banner1} />
 
-      <StyledLogements>
-        {logements.slice(0, 6).map((logement) => (
-          <Card
-            key={logement.id}
-            idLogement={logement.id}
-            picture={logement.pictures[0]}
-            title={logement.title}
-          />
-        ))}
-      </StyledLogements>
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : error ? (
+        <span>Error: {error.message}</span>
+      ) : (
+        <StyledLogements>
+          {logements &&
+            logements
+              .slice(0, 6)
+              .map((logement) => (
+                <Card
+                  key={logement.id}
+                  idLogement={logement.id}
+                  picture={logement.pictures[0]}
+                  title={logement.title}
+                />
+              ))}
+        </StyledLogements>
+      )}
     </StyledHome>
   )
 }
---
+++ src/utils/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+
+function useFetch(url) {
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    if (!url) return
+
+    setLoading(true)
+    setError(null)
+
+    async function fetchData() {
+      try {
+        const response = await fetch(url)
+        console.log(response)
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const data = await response.json()
+        setData(data)
+      } catch (err) {
+        setError(err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [url])
+
+  return { isLoading, data, error }
+}
+
+export default useFetch

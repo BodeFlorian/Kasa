032c49866f1e02fb12a69773c6a2ff208751aa58
Florian Bode <florianbode59@gmail.com>
2024-07-24 15:56:37 +0200

    About page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,5 +1,40 @@
+import useFetch from '../../utils/hooks/useFetch'
+import styled from 'styled-components'
+
+import Banner from '../../components/Banner'
+import Collapse from '../../components/Collapse'
+
+import banner2 from '../../assets/img/Banner/banner2.png'
+
+const StyledAbout = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+`
+
 function About() {
-  return <></>
+  const { data, isLoading, error } = useFetch('/datas/about.json')
+
+  return (
+    <StyledAbout>
+      <Banner picture={banner2} />
+      <div className="about">
+        {isLoading ? (
+          <span>Loading...</span>
+        ) : error ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          data &&
+          data.map((about, index) => (
+            <Collapse key={`about-${index}`} summary={about.summary}>
+              <p>{about.content}</p>
+            </Collapse>
+          ))
+        )}
+      </div>
+    </StyledAbout>
+  )
 }
 
 export default About
